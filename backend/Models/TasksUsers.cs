41dbaeb00ed016ebfcc40ef39d9b12f26e567eb6
hugo <hugo.resennde1123@gmail.com>
2025-03-23 15:00:14 -0300

    feat(created models): created a bases to models

---
+++ backend/Models/TasksUsers.cs
@@ -0,0 +1,33 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace backend.Models;
+
+public class UserTasks
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.String)]
+    public Guid UserId { get; set; }
+
+    [BsonElement("tasks")]
+    public List<TaskModel> Tasks { get; set; } = new List<TaskModel>();
+}
+
+public class TaskModel
+{
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; } = string.Empty;
+
+    [BsonElement("objetivo")]
+    public string Objetivo { get; set; } = string.Empty;
+
+    [BsonElement("notas")]
+    public string Notas { get; set; } = string.Empty;
+
+    [BsonElement("feito")]
+    public bool Feito { get; set; } = false;
+
+    [BsonElement("createdAt")]
+    [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
