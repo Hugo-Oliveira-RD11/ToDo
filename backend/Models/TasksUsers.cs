03de867b27e360b4481e1ce1e9d46b56adaac5d7
Hugo de Oliveira Resende <hugo.resennde1123@gmail.com>
2025-03-25 19:58:50 -0300

    refactor(mongodb model): from one document per user to one document per task

--- backend/Models/TasksUsers.cs
+++ backend/Models/TasksUsers.cs
@@ -2,30 +2,37 @@ using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace backend.Models;
-
+public enum Category {
+    White = 0,
+    Blue = 1,
+    Yellow = 2,
+    Red = 3
+}
 public class TasksUsers
 {
     [BsonId]
-    [BsonRepresentation(BsonType.String)]
-    public Guid UserId { get; set; }
-
-    [BsonElement("tasks")]
-    public List<TaskModel> Tasks { get; set; } = new List<TaskModel>();
-}
-
-public class TaskModel
-{
     [BsonRepresentation(BsonType.ObjectId)]
     public string Id { get; set; } = string.Empty;
 
+    [BsonRepresentation(BsonType.String)]
+    public Guid UserId { get; set; }
+    
     [BsonElement("objetivo")]
     public string Objetivo { get; set; } = string.Empty;
 
     [BsonElement("notas")]
     public string Notas { get; set; } = string.Empty;
 
+    [BsonRepresentation(BsonType.Int32)]
+    [BsonElement("category")]
+    public Category Category { get; set; } = Category.White;
+    
     [BsonElement("feito")]
     public bool Feito { get; set; } = false;
+
+    [BsonElement("aDayToComplet")]
+    [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
+    public DateTime? ADayToComplet { get; set; } = null;
 
     [BsonElement("createdAt")]
     [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
