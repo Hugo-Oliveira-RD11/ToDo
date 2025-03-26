94ba8d3a46e0be4c96d62f960720b0d4d7e4557c
hugo <hugo.resennde1123@gmail.com>
2025-03-26 16:31:02 -0300

    refactor(TaskService): add best returns and use LINQ

--- backend/Services/TaskService.cs
+++ backend/Services/TaskService.cs
@@ -1,30 +1,50 @@
 using backend.DTO;
 using backend.Models;
-
 using Microsoft.Extensions.Options;
-
 using MongoDB.Driver;
-
-using TasksUsersDTO = backend.DTO.TasksUsersDTO;
 
 namespace backend.Services;
 
 public class TaskService
 {
     private readonly IMongoCollection<TasksUsers> _tasksUsers;
+
+    private readonly UserService _userService;
+
     public TaskService(
-        IOptions<TasksUsersDatabaseSettings> tasksDatabase
+        IOptions<TasksUsersDatabaseSettings> tasksDatabase, UserService userServices
         )
     {
+        _userService = userServices;
         var mongoConnection = new MongoClient(tasksDatabase.Value.ConnectionString);
         var mongoDatabase = mongoConnection.GetDatabase(tasksDatabase.Value.DatabaseName);
         _tasksUsers = mongoDatabase.GetCollection<TasksUsers>(tasksDatabase.Value.DatabaseName);
     }
 
-    public async Task<List<TasksUsersDTO>> GetAllTasksByUser(Guid userId)
+    public List<TasksUsersDTO> GetAllTasksByUser(Guid userId)
     {
 
-        var tasksDTO =_tasksUsers.AsQueryable().Where(t => t.UserId == userId).Select(t => new TasksUsersDTO
+        var listTask = _tasksUsers.AsQueryable()
+            .Where(t => t.UserId == userId)
+            .Select(t => new TasksUsersDTO
+            {
+                Id = t.Id,
+                Feito = t.Feito,
+                Notas = t.Notas,
+                Objetivo = t.Objetivo,
+                ADayToComplet = t.ADayToComplet,
+                Category = t.Category
+            })
+            .OrderBy(t => t.Objetivo)
+            .ThenBy(t => t.Feito)
+            .ToList();
+
+        return listTask;
+    }
+
+    public TasksUsersDTO GetTaskById(string id)
+    {
+        var listTask = _tasksUsers.AsQueryable().Where(t => t.Id == id).Select(t => new TasksUsersDTO
         {
             Id = t.Id,
             Feito = t.Feito,
@@ -32,31 +52,69 @@ public class TaskService
             Objetivo = t.Objetivo,
             ADayToComplet = t.ADayToComplet,
             Category = t.Category
-        }).ToList();
+        }).FirstOrDefault();
 
-        return tasksDTO;
+        return listTask;
+
+    }
+    public List<TasksUsersDTO> GetAllTasksToday(Guid userId)
+    {
+        var listTask = _tasksUsers.AsQueryable()
+            .Where(t => t.UserId == userId && t.ADayToComplet == DateTime.Today)
+            .Select(t => new TasksUsersDTO
+            {
+                Id = t.Id,
+                Feito = t.Feito,
+                Notas = t.Notas,
+                Objetivo = t.Objetivo,
+                ADayToComplet = t.ADayToComplet,
+                Category = t.Category
+            })
+            .OrderBy(t => t.Objetivo)
+            .ThenBy(t => t.Feito)
+            .ToList();
+
+        return listTask;
     }
 
-    public TasksUsersDTO GetTaskByUser(string id) {
-        var taskDTO = _tasksUsers.AsQueryable().Where(t => t.Id == id).Select(t => new TasksUsersDTO{
-            Id = t.Id,
-            Feito = t.Feito,
-            Notas = t.Notas,
-            Objetivo = t.Objetivo,
-            ADayToComplet = t.ADayToComplet,
-            Category = t.Category
-            }).FirstOrDefault();
+    public async Task<TasksUsersDTO?> CreateAsync(TasksUsers newTask)
+    {
+        if(_userService.GetUserById(newTask.UserId) == null)
+            return null;
 
-        return taskDTO;
-        
+        await  _tasksUsers.InsertOneAsync(newTask);
+        var task = new TasksUsersDTO
+        {
+                Id = newTask.Id,
+                Feito = newTask.Feito,
+                Notas = newTask.Notas,
+                Objetivo = newTask.Objetivo,
+                ADayToComplet = newTask.ADayToComplet,
+                Category = newTask.Category
+        };
+
+        return task;
     }
 
-    public async Task CreateAsync(TasksUsers newTask) =>
-        await _tasksUsers.InsertOneAsync(newTask);
+    public async Task<TasksUsersDTO?> UpdateAsync(string id, TasksUsers updatedTask)  
+    {
+        if(_userService.GetUserById(updatedTask.UserId) == null)
+            return null;
 
-    public async Task UpdateAsync(string id, TasksUsers updatedTask) =>
         await _tasksUsers.ReplaceOneAsync(x => x.Id == id, updatedTask);
 
-    public async Task RemoveAsync(string id) =>
+        var task = new TasksUsersDTO
+        {
+                Id = updatedTask.Id,
+                Feito = updatedTask.Feito,
+                Notas = updatedTask.Notas,
+                Objetivo = updatedTask.Objetivo,
+                ADayToComplet = updatedTask.ADayToComplet,
+                Category = updatedTask.Category
+        };
+
+        return task;
+    }
+    public async Task RemoveAsync(string id) => // nao vejo o que retornar
         await _tasksUsers.DeleteOneAsync(x => x.Id == id);
 }
