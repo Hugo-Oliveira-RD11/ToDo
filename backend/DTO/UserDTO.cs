ade6f07a189756eb43abd70b38da856667b86b43
hugo <hugo.resennde1123@gmail.com>
2025-03-26 16:59:50 -0300

    fix(error merge in emacs):

--- backend/DTO/UserDTO.cs
+++ backend/DTO/UserDTO.cs
@@ -1,9 +1,6 @@
 namespace backend.DTO;
 
 public class UserDTO{
-    public Guid Id {get; set;}
-    public string Name {get; set;}
-    public string Email { get; set; }
     public Guid? Id {get; set;}
     public string? Name {get; set;}
     public string? Email { get; set; }
