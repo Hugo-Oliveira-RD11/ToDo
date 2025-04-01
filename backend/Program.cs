3afa5e6430c5a48d8147b6d322b9493fd61187eb
hugo <hugo.resennde1123@gmail.com>
2025-04-01 15:00:29 -0300

    choro(database update ): database update in application start

--- backend/Program.cs
+++ backend/Program.cs
@@ -24,6 +24,12 @@ builder.Services.AddScoped<UserService>();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<UserContext>();
+    db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
