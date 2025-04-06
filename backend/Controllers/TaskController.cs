a40335c508fec487499c01358778a336c4a86bfc
hugo <hugo.resennde1123@gmail.com>
2025-04-06 11:34:12 -0300

    refactor(Controllers): dependency injection

--- backend/Controllers/TaskController.cs
+++ backend/Controllers/TaskController.cs
@@ -1,7 +1,7 @@
 
 using backend.DTO;
 using backend.Models;
-using backend.Services;
+using backend.Services.TaskServices;
 using Microsoft.AspNetCore.Mvc;
 
 namespace backend.Controllers;
@@ -9,8 +9,8 @@ namespace backend.Controllers;
 [Route("v1/task")]
 public class TaskController : ControllerBase
 {
-    private readonly TaskService _taskService;
-    public TaskController(TaskService taskService)
+    private readonly ITaskService _taskService;
+    public TaskController(ITaskService taskService)
     {
         _taskService = taskService;
     }
