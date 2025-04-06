b72fac9b3c5916a7ce19571cb296788673aba159
hugo <hugo.resennde1123@gmail.com>
2025-04-06 11:19:54 -0300

    refactor(DTOs): changed to record

--- backend/DTO/TasksUsersDTO.cs
+++ backend/DTO/TasksUsersDTO.cs
@@ -2,12 +2,11 @@ using backend.Models;
 
 namespace backend.DTO;
 
-public class TasksUsersDTO
-{
-    public string Id { get; set; } = string.Empty;
-    public string Objetivo { get; set; } = string.Empty;
-    public string Notas { get; set; } = string.Empty;
-    public bool Feito { get; set; } = false;
-    public Category Category { get; set; } = Category.White;
-    public DateTime? ADayToComplet { get;set; } = null;
-}
+public record TasksUsersDTO(
+    string? Id,
+    string? Objetivo,
+    string? Notas,
+    bool? Feito,
+    Category Category,
+    DateTime? ADayToComplet
+);
--- backend/DTO/UserDTO.cs
+++ backend/DTO/UserDTO.cs
@@ -1,7 +1,7 @@
 namespace backend.DTO;
 
-public class UserDTO{
-    public Guid? Id {get; set;} = null;
-    public string? Name {get; set;} = string.Empty;
-    public string? Email { get; set; } = string.Empty;
-}
+public record UserDTO(
+    Guid? Id,
+    string? Name,
+    string? Email
+);
