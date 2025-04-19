568cc6f681bfc4c7152ffd9618d2d96cfa1756b0
hugo <hugo.resennde1123@gmail.com>
2025-04-19 08:45:16 -0300

    feat(Infrastructure-TaskRepository): created a implementation of ITaskRepository

---
+++ src/Todo.Infrastructure/Repositories/TaskRepository.cs
@@ -0,0 +1,67 @@
+using MongoDB.Driver;
+using Todo.Domain.Entities;
+using Todo.Domain.Interfaces.Repositories;
+using Todo.Infrastructure.Data.Model;
+using Todo.Infrastructure.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Todo.Infrastructure.Data.Repositories;
+
+public class TaskRepository : ITaskRepository
+{
+    private readonly IMongoCollection<TaskModel> _taskCollection;
+
+    public TaskRepository(IMongoDatabase database)
+    {
+        _taskCollection = database.GetCollection<TaskModel>("Tasks");
+    }
+
+    public async Task<Task> GetByIdAsync(string id)
+    {
+        var taskModel = await _taskCollection
+            .Find(t => t.Id == id)
+            .FirstOrDefaultAsync();
+
+        return taskModel != null ? TaskMapping.ToTask(taskModel) : null;
+    }
+
+    public async Task<IEnumerable<Task>> GetAllAsync()
+    {
+        var taskModels = await _taskCollection
+            .Find(t => true)
+            .ToListAsync();
+
+        return taskModels.Select(TaskMapping.ToTask);
+    }
+
+    public async Task AddAsync(Task task)
+    {
+        var taskModel = TaskMapping.ToTaskModel(task);
+        await _taskCollection.InsertOneAsync(taskModel);
+    }
+
+    public async Task UpdateAsync(Task task)
+    {
+        var taskModel = TaskMapping.ToTaskModel(task);
+        var filter = Builders<TaskModel>.Filter.Eq(t => t.Id, task.Id);
+        await _taskCollection.ReplaceOneAsync(filter, taskModel);
+    }
+
+    public async Task DeleteAsync(string id)
+    {
+        var filter = Builders<TaskModel>.Filter.Eq(t => t.Id, id);
+        await _taskCollection.DeleteOneAsync(filter);
+    }
+
+    public async Task<Task> GetByUserIdAsync(Guid userId)
+    {
+        var taskModel = await _taskCollection
+            .Find(t => t.UserId == userId)
+            .FirstOrDefaultAsync();
+
+        return taskModel != null ? TaskMapping.ToTask(taskModel) : null;
+    }
+}
