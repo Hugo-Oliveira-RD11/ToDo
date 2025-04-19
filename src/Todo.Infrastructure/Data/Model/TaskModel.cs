30265320a9706372cb3e28d12f54fd94adda32e7
hugo <hugo.resennde1123@gmail.com>
2025-04-19 08:42:51 -0300

    refactor(TaskModel): changed a name

--- src/Todo.Infrastructure/Data/Model/TaskModel.cs
+++ src/Todo.Infrastructure/Data/Model/TaskModel.cs
@@ -3,7 +3,7 @@ using MongoDB.Bson.Serialization.Attributes;
 
 namespace Todo.Infrastructure.Data.Model;
 
-public class TasksModel
+public class TaskModel
 {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
