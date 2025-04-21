ccee8a9110dc6161304340bac5ed0c9be513e2c8
hugo <hugo.resennde1123@gmail.com>
2025-04-21 14:44:50 -0300

    refactor(Domain-User): change a type Task to TodoTask

--- src/Todo.Domain/Interfaces/Repositories/ITaskRepository.cs
+++ src/Todo.Domain/Interfaces/Repositories/ITaskRepository.cs
@@ -1,14 +1,11 @@
 using Todo.Domain.Entities;
-using System;
-using System.Threading.Tasks;
-using System.Collections.Generic;
 
 namespace Todo.Domain.Interfaces.Repositories;
 public interface ITaskRepository
 {
-    Task<Task> GetTaskByIdAsync(string id);
-    Task<IEnumerable<Task>> GetAllAsync();
-    Task AddAsync(Task task);
-    Task UpdateAsync(Task task);
+    Task<TodoTask> GetTaskByIdAsync(string id);
+    Task<IEnumerable<TodoTask>> GetAllAsync();
+    Task AddAsync(TodoTask todoTask);
+    Task UpdateAsync(TodoTask todoTask);
     Task DeleteAsync(string id);
 }
