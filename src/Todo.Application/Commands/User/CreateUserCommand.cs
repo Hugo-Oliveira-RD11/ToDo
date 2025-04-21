5728ac49a70b11394b04063cac28b8625e4b04db
hugo <hugo.resennde1123@gmail.com>
2025-04-21 17:04:18 -0300

    fix(Appli-CreateUserCommand): change Email ValueObject to string

--- src/Todo.Application/Commands/User/CreateUserCommand.cs
+++ src/Todo.Application/Commands/User/CreateUserCommand.cs
@@ -4,10 +4,10 @@ namespace Todo.Application.Commands.User;
 public class CreateUserCommand
 {
     public string Name { get; init; }
-    public Email Email { get; init; } 
+    public string Email { get; init; } 
     public string Password { get; init; }
 
-    public CreateUserCommand(string name, Email email, string password )
+    public CreateUserCommand(string name, string email, string password )
     {
         Name = name;
         Email = email;
