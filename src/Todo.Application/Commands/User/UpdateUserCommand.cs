3fb2a467363ff987af9c5734a49e4c08693c08cb
hugo <hugo.resennde1123@gmail.com>
2025-04-21 17:06:35 -0300

    feat(Appli-UpdateUserCommand): add nullable

--- src/Todo.Application/Commands/User/UpdateUserCommand.cs
+++ src/Todo.Application/Commands/User/UpdateUserCommand.cs
@@ -3,11 +3,11 @@ namespace Todo.Application.Commands.User;
 
 public class UpdateUserCommand
 {
-    public string Name { get; init; }
-    public Email Email { get; init; }
-    public string Password { get; init; }
+    public string? Name { get; init; }
+    public string? Email { get; init; }
+    public string? Password { get; init; }
 
-    public UpdateUserCommand(string? name = string.Empty, Email? email = Email.Empty, string? password = string.Empty )
+    public UpdateUserCommand(string? name = null, string? email = null, string? password = null )
     {
         Name = name;
         Email = email;
