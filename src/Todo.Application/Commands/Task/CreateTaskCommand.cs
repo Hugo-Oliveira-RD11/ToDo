157179b6f57e34d66e5ae62891326420542cb553
hugo <hugo.resennde1123@gmail.com>
2025-04-21 17:02:13 -0300

    fix(Appli-CreateTaskCommand): wrong names

--- src/Todo.Application/Commands/Task/CreateTaskCommand.cs
+++ src/Todo.Application/Commands/Task/CreateTaskCommand.cs
@@ -1,20 +1,23 @@
 
+using Todo.Domain.Enums;
+using Todo.Domain.ValueObjects;
+
 namespace Todo.Application.Commands.Task;
 
 public class CreateTaskCommand
 {
     public string Goal { get; init; }
     public string Notes { get; init; }
-    public Category Category { get; init; }
+    public Categories Category { get; init; }
     public bool Done { get; init; } 
-    public DueDate ADayToComplet { get; init ; }
+    public DueDate CompletationDate { get; init ; }
 
-    public CreateTaskCommand(string goal, string notes, Category category, bool done, DueDate aDayToComplet)
+    public CreateTaskCommand(string goal, string notes, Categories category, bool done, DueDate completationDate)
     {
         Goal = goal;
         Notes = notes;
         Category = category;
         Done = done;
-        ADayToComplet = aDayToComplet;
+        CompletationDate = completationDate;
     }
 }
