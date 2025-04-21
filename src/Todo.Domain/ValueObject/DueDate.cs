677352c24e331da07c8e22adde1278d5c0695534
hugo <hugo.resennde1123@gmail.com>
2025-04-21 00:09:44 -0300

    style(Domain-ValueObject): remove brackts of namespace

--- src/Todo.Domain/ValueObject/DueDate.cs
+++ src/Todo.Domain/ValueObject/DueDate.cs
@@ -1,31 +1,30 @@
-namespace Todo.Domain.ValueObjects
+namespace Todo.Domain.ValueObjects;
+
+public class DueDate
 {
-    public class DueDate
+    public DateTime Value { get; }
+
+    private DueDate() => Value = DateTime.MinValue;
+
+    public DueDate(DateTime value)
     {
-        public DateTime Value { get; }
+        if (value.Date < DateTime.UtcNow.Date)
+            throw new ArgumentException("A data de conclusão não pode ser no passado.");
 
-        private DueDate() => Value = DateTime.MinValue;
-
-        public DueDate(DateTime value)
-        {
-            if (value.Date < DateTime.UtcNow.Date)
-                throw new ArgumentException("A data de conclusão não pode ser no passado.");
-
-            Value = value;
-        }
-
-        public static DueDate Empty => new DueDate();
-
-        public override bool Equals(object? obj) =>
-            obj is DueDate other && Value.Equals(other.Value);
-
-        public bool Equals(DueDate? other) => other is not null && Value.Equals(other.Value);
-
-        public override int GetHashCode() => Value.GetHashCode();
-
-        public override string ToString() => Value.ToString("yyyy-MM-dd");
-
-        public static bool operator ==(DueDate? left, DueDate? right) => Equals(left, right);
-        public static bool operator !=(DueDate? left, DueDate? right) => !Equals(left, right);
+        Value = value;
     }
+
+    public static DueDate Empty => new DueDate();
+
+    public override bool Equals(object? obj) =>
+        obj is DueDate other && Value.Equals(other.Value);
+
+    public bool Equals(DueDate? other) => other is not null && Value.Equals(other.Value);
+
+    public override int GetHashCode() => Value.GetHashCode();
+
+    public override string ToString() => Value.ToString("yyyy-MM-dd");
+
+    public static bool operator ==(DueDate? left, DueDate? right) => Equals(left, right);
+    public static bool operator !=(DueDate? left, DueDate? right) => !Equals(left, right);
 }
