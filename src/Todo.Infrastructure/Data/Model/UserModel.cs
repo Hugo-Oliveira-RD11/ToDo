afeba524796c9bed6b8d6b8e541e4717d6dcc933
hugo <hugo.resennde1123@gmail.com>
2025-04-21 14:52:35 -0300

    refactor(Infra-UserModel): change fluentapi to data annotaion

--- src/Todo.Infrastructure/Data/Model/UserModel.cs
+++ src/Todo.Infrastructure/Data/Model/UserModel.cs
@@ -1,10 +1,21 @@
-using Mapster;
+using System.ComponentModel.DataAnnotations;
+
 using Todo.Domain.Entities;
 
+namespace Todo.Infrastructure.Data.Model;
 public class UserModel
 {
+    [Key]
     public Guid Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
+    
+    [StringLength(250, MinimumLength = 3)]
+    public string Name { get; set; }
+    
+    [DataType(DataType.EmailAddress)]
+    [Required]
+    public string Email { get; set; } 
+    
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; } 
 }
