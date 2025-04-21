8babe6b7262a3130390a9eade9aeb172dfbdbf8b
hugo <hugo.resennde1123@gmail.com>
2025-04-21 14:45:58 -0300

    feat(Domain-User): remove from the published version the method get all user

--- src/Todo.Domain/Interfaces/Repositories/IUserRepository.cs
+++ src/Todo.Domain/Interfaces/Repositories/IUserRepository.cs
@@ -3,11 +3,14 @@ using System;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 
+
 namespace Todo.Domain.Interfaces.Repositories;
 public interface IUserRepository
 {
     Task<User> GetByIdAsync(Guid id);
+#if DEBUG
     Task<IEnumerable<User>> GetAllAsync(); 
+#endif
     Task AddAsync(User user); 
     Task UpdateAsync(User user); 
     Task DeleteAsync(Guid id); 
