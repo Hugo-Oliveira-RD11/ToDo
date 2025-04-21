a5e325d7c44e3436ec2d59c2112ce5edb6ecfa0d
hugo <hugo.resennde1123@gmail.com>
2025-04-21 00:10:17 -0300

    style(Infrastructure-Mapping): remove brackts of namespace

--- src/Todo.Infrastructure/Mapping/TaskMapping.cs
+++ src/Todo.Infrastructure/Mapping/TaskMapping.cs
@@ -1,36 +1,36 @@
 using Todo.Domain.Entities;
 using Todo.Infrastructure.Data.Model;
 
-namespace Todo.Infrastructure.Mapping
+namespace Todo.Infrastructure.Mapping;
+
+public static class TaskMapping
 {
-    public static class TaskMapping
+    public static TaskModel ToTaskModel(Task task)
     {
-        public static TaskModel ToTaskModel(Task task)
+        return new TaskModel
         {
-            return new TaskModel
-            {
-                Id = task.Id,
-                UserId = task.UserId,
-                Goal = task.Goal,
-                Notes = task.Notes,
-                Category = task.Category,
-                Done = task.Done,
-                ADayToComplet = task.ADayToComplet,
-                CreatedAt = DateTime.UtcNow
-            };
-        }
-
-        public static Task ToTask(TaskModel taskModel)
-        {
-            var task = new Task(
-                taskModel.UserId,
-                taskModel.Goal,
-                taskModel.Notes,
-                taskModel.Category,
-                taskModel.ADayToComplet);
-
-            task.UpdateDayToComplete(taskModel.ADayToComplet);
-            return task;
-        }
+            Id = task.Id,
+            UserId = task.UserId,
+            Goal = task.Goal,
+            Notes = task.Notes,
+            Category = task.Category,
+            Done = task.Done,
+            ADayToComplet = task.ADayToComplet,
+            CreatedAt = DateTime.UtcNow
+        };
     }
+
+    public static Task ToTask(TaskModel taskModel)
+    {
+        var task = new Task(
+            taskModel.UserId,
+            taskModel.Goal,
+            taskModel.Notes,
+            taskModel.Category,
+            taskModel.ADayToComplet);
+
+        task.UpdateDayToComplete(taskModel.ADayToComplet);
+        return task;
+    }
+
 }
