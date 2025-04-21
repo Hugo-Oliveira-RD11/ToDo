0d67c64d4216db06522fdef4846dfbea2b262f51
hugo <hugo.resennde1123@gmail.com>
2025-04-21 16:57:27 -0300

    feat(Appli-TodoTaskMappingDTO): rename TaskMappingDTO to TodoTaskMappingDTO

---
+++ src/Todo.Application/Mapping/TodoTaskMappingDTO.cs
@@ -0,0 +1,61 @@
+using Todo.Application.DTOs;
+using Todo.Domain.Entities;
+
+namespace Todo.Application.Mapping;
+
+public static class TodoTaskMappingDTO
+{
+    public static TaskDTO ToTaskDto(TodoTask task)
+    {
+        return new TaskDTO
+        {
+            Id = task.Id,
+            Goal = task.Goal,
+            Done = task.Done,
+            Notes = task.Notes,
+            Category = task.Category,
+            CompletationDate = task.CompletationDate
+        };
+    }
+
+    public static TodoTask ToTask(TaskDTO taskDto, Guid userId)
+    {
+        var task = TodoTask.LoadFromDb(
+            id: taskDto.Id,
+            userId: userId,
+            goal: taskDto.Goal,
+            notes: taskDto.Notes,
+            category: taskDto.Category,
+            completationDate: taskDto.CompletationDate.Value,
+            done: taskDto.Done 
+            );
+        
+        return task;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
