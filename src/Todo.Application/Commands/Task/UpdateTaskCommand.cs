661c0d082d0386566b6b129e7c1f842eadf8d84a
hugo <hugo.resennde1123@gmail.com>
2025-04-21 17:02:39 -0300

    fix(Appli-UpdateTaskCommand): wrong names and not nullable

--- src/Todo.Application/Commands/Task/UpdateTaskCommand.cs
+++ src/Todo.Application/Commands/Task/UpdateTaskCommand.cs
@@ -1,19 +1,22 @@
+using Todo.Domain.Enums;
+using Todo.Domain.ValueObjects;
+
 namespace Todo.Application.Commands.Task;
 
 public class UpdateTaskCommand
 {
-    public string Goal { get; init; } 
-    public string Notes { get; init; } 
-    public Category Category { get; init; } 
-    public bool Done { get; init; } 
-    public DueDate ADayToComplet { get; init; }
+    public string? Goal { get; init; } 
+    public string? Notes { get; init; } 
+    public Categories? Category { get; init; } 
+    public bool? Done { get; init; } 
+    public DueDate? CompletationDate { get; init; }
 
-    public UpdateTaskCommand(string? goal = string.Empty, string? notes = string.Empty, Category? category = null, bool? done = false, DueDate? aDayToComplet = null)
+    public UpdateTaskCommand(string? goal = null, string? notes = null, Categories? category = null, bool? done = false, DueDate? completationDate = null)
     {
         Goal = goal;
         Notes = notes;
         Category = category;
         Done = done;
-        ADayToComplet = aDayToComplet;
+        CompletationDate = completationDate;
     }
 }
