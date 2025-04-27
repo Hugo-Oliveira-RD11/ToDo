851239c3c7534cf45f599478e110937f72a6896c
hugo <hugo.resennde1123@gmail.com>
2025-04-27 07:56:05 -0300

    feat(Domain-ITaskRepository): add userId to GetAllTask

--- src/Todo.Domain/Interfaces/Repositories/ITaskRepository.cs
+++ src/Todo.Domain/Interfaces/Repositories/ITaskRepository.cs
@@ -6,7 +6,7 @@ public interface ITaskRepository
     Task<IEnumerable<TodoTask?>> GetByUserIdPagedAsync(Guid userId, int pageNumber, int pageSize);
     Task<TodoTask?> GetTaskByIdAndUserIdAsync(string id, Guid userId);
     Task<TodoTask?> GetTaskByGoalAndUserIdAsync(string goal, Guid userId);
-    Task<IEnumerable<TodoTask>> GetAllAsync();
+    Task<IEnumerable<TodoTask>> GetAllAsync(Guid userId);
     Task AddAsync(TodoTask todoTask);
     Task UpdateAsync(TodoTask todoTask);
     Task DeleteAsync(string id);
