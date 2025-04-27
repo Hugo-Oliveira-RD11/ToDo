8c0897ff57e8c3f714d894249c50c82097918965
hugo <hugo.resennde1123@gmail.com>
2025-04-27 07:52:40 -0300

    style(App-GetTasksQuery): change name of property

--- src/Todo.Application/Queries/GetTasksQuery.cs
+++ src/Todo.Application/Queries/GetTasksQuery.cs
@@ -3,7 +3,7 @@ namespace Todo.Application.Queries;
 public class GetTasksQuery
 {
     public Guid UserId { get; init; }
-    public int Page { get; init; }
+    public int PageNumber { get; init; }
     public int PageSize { get; init;}
 
   }
