0f84c5f4b8bb6714f6a3c09c75bd42b87e33af1f
hugo <hugo.resennde1123@gmail.com>
2025-04-26 22:23:10 -0300

    feat(Domain-ITaskRepository): create new methods to improve task selection

--- src/Todo.Domain/Interfaces/Repositories/ITaskRepository.cs
+++ src/Todo.Domain/Interfaces/Repositories/ITaskRepository.cs
@@ -3,7 +3,9 @@ using Todo.Domain.Entities;
 namespace Todo.Domain.Interfaces.Repositories;
 public interface ITaskRepository
 {
-    Task<TodoTask> GetTaskByIdAsync(string id);
+    Task<IEnumerable<TodoTask?>> GetByUserIdPagedAsync(Guid userId, int pageNumber, int pageSize);
+    Task<TodoTask?> GetTaskByIdAsync(string id);
+    Task<TodoTask?> GetTaskByGoalAsync(string goal);
     Task<IEnumerable<TodoTask>> GetAllAsync();
     Task AddAsync(TodoTask todoTask);
     Task UpdateAsync(TodoTask todoTask);
