22727a55e559a8e3f8c82a450416e36f2ad12975
hugo <hugo.resennde1123@gmail.com>
2025-05-10 12:46:11 -0300

    feat(Domain-Result): update to Result for return only

--- src/Todo.Domain/Shared/Result.cs
+++ src/Todo.Domain/Shared/Result.cs
@@ -7,5 +7,6 @@ public class Result<T>
     public T Data { get; private set; }
 
     public static Result<T> Ok(T data) => new Result<T> { Success = true, Data = data };
+    public static Result<T> Ok() => new Result<T> { Success = true };
     public static Result<T> Fail(string error) => new Result<T> { Success = false, Error = error };
 }
