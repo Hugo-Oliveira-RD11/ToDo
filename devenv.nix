db66019c96f739c117bd411d80715b60357bd23c
Hugo de Oliveira Resende <hugo.resennde1123@gmail.com>
2026-02-14 15:49:01 -0300

    chore: trocando de devbox para devenv
    motivo: prefiro algo que seja mais nix do que as declaracoes do devbox
    mas assumo que tive dificuldade para rodar o lsp, por isso tive que
    combinar os pacotes

---
+++ devenv.nix
@@ -0,0 +1,66 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  # env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = with pkgs;
+    [
+      git
+      csharp-ls
+      (dotnetCorePackages.combinePackages [
+      dotnet-sdk_10
+      dotnet-sdk_9
+      ])
+    ];
+
+  # https://devenv.sh/languages/
+  languages = {
+    # dotnet = {
+    #   enable = true;
+    #   package = pkgs.dotnet-sdk_9;
+    #   lsp.enable = true;
+    #   lsp.package = pkgs.omnisharp-roslyn;
+    # };
+    nix ={
+      enable = true;
+      #lsp.enable = true;
+    };
+  };
+
+  # https://devenv.sh/processes/
+  # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+  # scripts.hello.exec = ''
+  #   echo hello from $GREET
+  # '';
+
+  # https://devenv.sh/basics/
+  enterShell = ''
+    # hello         # Run scripts directly
+    # git --version # Use packages
+    dotnet --list-sdks
+  '';
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+  # enterTest = ''
+  #   echo "Running tests"
+  #   git --version | grep --color=auto "${pkgs.git.version}"
+  # '';
+
+  # https://devenv.sh/git-hooks/
+  # git-hooks.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
