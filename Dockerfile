497c002afff38598d8725008c0128f73b299934f
hugo <hugo.resennde1123@gmail.com>
2026-03-31 18:19:38 -0300

    feat: make run
    so queria fazer rodar e criar um endpoint simples
    para mostrar qual e o id do container

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,21 @@ FROM mcr.microsoft.com/dotnet/aspnet:9.0 AS base
 WORKDIR /app
 EXPOSE 80
 
+ENV DOTNET_SYSTEM_NET_DISABLEIPV6=1
+
 FROM mcr.microsoft.com/dotnet/sdk:9.0 AS build
 WORKDIR /src
+
+ENV DOTNET_SYSTEM_NET_DISABLEIPV6=1
+
+COPY Directory.Packages.props .
 
 COPY ["src/Todo.Domain/Todo.Domain.csproj", "src/Todo.Domain/"]
 COPY ["src/Todo.Application/Todo.Application.csproj", "src/Todo.Application/"]
 COPY ["src/Todo.Infrastructure/Todo.Infrastructure.csproj", "src/Todo.Infrastructure/"]
 COPY ["src/UI/Todo.Api/Todo.Api.csproj", "src/UI/Todo.Api/"]
+
+RUN dotnet nuget locals all --clear
 
 RUN dotnet restore "src/UI/Todo.Api/Todo.Api.csproj"
 
--- src/UI/Todo.Api/Program.cs
+++ src/UI/Todo.Api/Program.cs
@@ -1,7 +1,5 @@
-
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.EntityFrameworkCore;
-using HealthChecks.MongoDb;
-using HealthChecks.NpgSql;
 
 using MongoDB.Driver;
 
@@ -24,28 +22,32 @@ builder.Services.AddUserModele();
 builder.Services.AddTodoTaskModule();
 
 builder.Services.AddHealthChecks()
-    .AddCheck("self", () => Microsoft.Extensions.Diagnostics.HealthChecks.HealthCheckResult.Healthy())
-    .AddNpgSql(
-        connectionString: builder.Configuration["ConnectionsDB:UserConnection"]!,
-        name: "Postgres",
-        tags: new[] { "database", "sql" })
-    .AddMongoDb( sp => new MongoClient(builder.Configuration["ConnectionsDB:TasksDatabase:ConnectionString"]),
-        name: "TaskDB-Mongo"
-        );
+    .AddNpgSql(builder.Configuration["ConnectionsDB:UserConnection"]!, name: "database")
+    .AddMongoDb(sp => new MongoClient(builder.Configuration["ConnectionsDB:TasksDatabase:ConnectionString"]!), name: "mongodb");
 
 var app = builder.Build();
 
+app.MapGet("/", () =>
+{
+
+    var hostmane = Environment.MachineName;
+    return $"Ola mundo {hostmane}";
+});
+
 using (var scope = app.Services.CreateScope())
 {
-    int maxRetries = 10, delayMilliseconds=5000;
+    int maxRetries = 10, delayMilliseconds = 5000;
 
     var db = scope.ServiceProvider.GetRequiredService<UserDbContext>();
-    for(int i=0; i< maxRetries;i++ ){
-        try{
+    for (int i = 0; i < maxRetries; i++)
+    {
+        try
+        {
             db.Database.Migrate();
             break;
         }
-        catch(Exception e){
+        catch (Exception e)
+        {
             Console.WriteLine($"error: {e}");
             Thread.Sleep(delayMilliseconds);
         }
@@ -59,11 +61,11 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.MapHealthChecks(
-    "/v1/health",new HealthCheckOptions()
-    {
-        Predicate = _ => true,
-        ReponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
-    });
+// app.MapHealthChecks(
+//     "/v1/health", new HealthCheckOptions()
+//     {
+//         Predicate = _ => true,
+//         ReponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+//     });
 
 app.Run();
